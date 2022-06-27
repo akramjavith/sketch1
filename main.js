bba19439609d29bd8a4c694a26a4c61202ec6a5e
javith <akramjavithkhan@gmail.com>
2022-06-27 09:53:43 +0530

    updated

--- main.js
+++ main.js
@@ -1,25 +1,21 @@
 const container = document.getElementById("grid-container");
 
-
 function random(number) {
     return Math.floor(Math.random() * number + 1);
 }
+
 function makeRows(rows, cols) {
     container.style.setProperty("--grid-rows", rows);
     container.style.setProperty("--grid-cols", cols);
-    for (i = 0; i < (rows * cols); i--) {
+    for (i = 0; i < (rows * cols); i++) {
         let cell = document.createElement("div");
         container.appendChild(cell).className = "grid-item";
         cell.addEventListener("mouseover", function bgChange() {
-           const rndCol = "rgb(" + random(255) + "," + random(255) + "," + random(255) + ")";
-         cell.style.backgroundColor = rndCol;
-            
+            const rndCol = "rgb(" + random(255) + "," + random(255) + "," + random(255) + ")";
+            cell.style.backgroundColor = rndCol;
         });
     };
 };
-
-
-
 
 makeRows(16,16);
 
